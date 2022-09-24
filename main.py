be88154b8a6a65e7c3937423ddaa85466cb26807
Syarif Ys <5yarif.y.s@gmail.com>
2022-09-24 14:21:11 +0800

    24 september 2022

---
+++ main.py
@@ -0,0 +1,15 @@
+while True:
+    q = input('Add (a) Search (s) Quit (q) : ')
+    if q == 'a':
+        with open('contact.txt', 'a') as f:
+            name = input('Name: ')
+            phone = input('Phone: ')
+            f.writelines((name,' : ', phone,'\n'))
+    elif q == 's':
+        with open ('contact.txt', 'r') as f:
+            search = input('Search: ')
+            for i in f:
+                if search in i:
+                    print(i)
+    else:
+        break
